b110b3045c76e3e7fe09758b69203e8c45577708
Immanuel Williams <jamesijw23@gmail.com>
2018-05-20 09:01:27 -0700

    Testing proper functionality of functions

---
+++ knn_testing.R
@@ -0,0 +1,12 @@
+## Testing Split Function
+s_df = split_df(iris,0.1)
+test_df = s_df$test_df %>% select(-Species)
+train_df = s_df$train_df %>% select(-Species)
+
+
+## Test of Distance Functions
+v1 = c(1,-1,3)
+v2 = c(4,2,3)
+
+euclideanDistance(v1,v2,2)
+absoluteDistance(v1,v2,2)
