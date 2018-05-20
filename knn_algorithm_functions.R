e8ddb4153e9eecb55a21e28c9ab1285f53377ef6
Immanuel Williams <jamesijw23@gmail.com>
2018-05-20 06:56:53 -0700

    Create a split function for test and training df

--- knn_algorithm_functions.R
+++ knn_algorithm_functions.R
@@ -1,14 +1,30 @@
+library(dplyr)
 ##--------------------------------------
 ## Functions
 ##--------------------------------------
 
 ##--------------------------------------
-## Name:
-## Input: 
-## Output:
-## Purpose:
+## Dependencies:
+## a. dplyr: df manipulation
 ##--------------------------------------
-split_df(df,split)
+##--------------------------------------
+## Name: split_df
+## Input: 
+## 1) df--> dataframe
+## 2) split --> percent of df to be the test 
+## Output: A list that contains train and test dfs
+## Purpose: Split the df into a training and test dfs for
+## validation purposes
+##--------------------------------------
+split_df = function(df,split){
+  n_df = nrow(df)
+  n_test = round(split*n_df)
+  rows_test = sample(n_df,n_test)
+  temp_df = data.frame(row_num = seq(1,n_df),df)
+  test_df = temp_df %>% filter(row_num %in% rows_test)
+  train_df = temp_df %>% filter(!(row_num %in% rows_test))
+  return(list(train_df = train_df,test_df = test_df))
+}
 
 
 
