2d2d716239ba9418e1b5a50ea0fa5011b8eea76b
Immanuel Williams <jamesijw23@gmail.com>
2018-05-23 08:53:35 -0700

    Square the distance matrix

---
+++ knn_algorithm_rcpp.cpp
@@ -0,0 +1,33 @@
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+//-----------------------------------
+// Name: repbyrow
+// Input: 
+// a) v1 --> Vector of values
+// b) num_row --> rnumber of rows repeat
+// Output: Matrix of vector repeated by row
+// Purpose: Repeat a vector into num_row times by rows
+// Improvements: 
+// a) No for loop 
+//-----------------------------------
+// [[Rcpp::export]]
+arma::mat repbyrow(arma::vec v1,int num_col){
+  arma::mat matrix_v1(v1.size(),num_col);
+  for(int i=0;i<num_col;i++){
+    matrix_v1.col(i) = v1;
+  }
+  return arma::trans(matrix_v1);
+}
+
+
+// [[Rcpp::export]]
+arma::mat euclidean_dist_matrix(arma::mat mat1,arma::vec v1) {
+  int num_rows = mat1.n_rows;
+  arma::mat mat2 = repbyrow(v1,num_rows);
+  return pow(mat1 - mat2,2);
+}
+
+
+
