91a6a9ca7f5be0950fa9285262b44730e2d2e2cc
Immanuel Williams <jamesijw23@gmail.com>
2018-05-23 09:18:58 -0700

    Create Cross Validation Outline

---
+++ Cross_Validation_1.R
@@ -0,0 +1,23 @@
+
+
+
+##### Cross Validation
+
+
+
+##### Type 1: Divide the Data into 2 sets (80/20)
+
+## b) Find Metrics
+
+
+##### Type 2: Divide the Data into 10 sets 
+
+## a) Split Data based on function (look at shipt)
+
+
+## b) Find Metrics based on doing analysis on 90% and tesing 10%
+
+
+## c) Plot Metric for each sample removed
+
+
