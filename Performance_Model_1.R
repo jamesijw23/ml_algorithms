8e51b40df51e61f4e0bd08a38ae4af8c9f9e64df
Immanuel Williams <jamesijw23@gmail.com>
2018-05-23 09:21:19 -0700

    Outline Metrics document

---
+++ Performance_Model_1.R
@@ -0,0 +1,22 @@
+## Classification Accuracy
+
+
+## Logaritmic Loss
+
+
+## COnfusion Matrix
+
+
+## Area Under Curve
+
+## F1 Score
+
+
+## F2 Score
+
+
+## Mean Absolute Erro
+
+
+
+## Mean Square Error
