6c61d277fc00ce0e05185ce4cc4d85043b0b7c89
Immanuel Williams <jamesijw23@gmail.com>
2018-05-29 06:51:39 -0700

    Outline to test crossvalidation

--- Testing_Cross_Validation.R
+++ Testing_Cross_Validation.R
@@ -1,6 +1,19 @@
+library(dplyr)
+library(tidyr)
 df = iris %>% select(Species,Sepal.Length,
                            Sepal.Width,Petal.Length,
                            Petal.Width )
 
+k = 3
+## Testing Split Function
+s_df = split_df(df,0.1)
+test_df = s_df$test_df  
+train_df = s_df$train_df 
 
-cross_validation_KNN(df,type_cv = 2,p_test=0.2,KN = 10)
+model1 = knn_cpp(train_df, test_df, k, distance='euclid')
+
+tr = test_df[,1]
+es = model1$predictions
+
+m = metrics(tr,es)
+long_m = gather(m,metrics_name,metric_value,TPR_metric:MKd_metric)
