dc06c628446476aba60a50c1e15ff447f0e93f92
Immanuel Williams <jamesijw23@gmail.com>
2018-05-29 06:50:21 -0700

    Ability to graph 4 measures for model performace

--- Plotting_Metrics_1.R
+++ Plotting_Metrics_1.R
@@ -1,24 +1,33 @@
 library(ggplot2)
+library(dplyr)
+library(tidyr)
+df = iris %>% select(Species,Sepal.Length,
+                     Sepal.Width,Petal.Length,
+                     Petal.Width )
 
-test_df = data.frame(metric1 = c(0.25,0.3), metric2 = c(-0.4,-0.2),levelnames=c("cat","dog"))
+k = 3
+## Testing Split Function
+s_df = split_df(df,0.1)
+test_df = s_df$test_df  
+train_df = s_df$train_df 
 
-ggplot(test_df,aes(x=))
+model1 = knn_cpp(train_df, test_df, k, distance='euclid')
 
+tr = test_df[,1]
+es = model1$predictions
 
-
-
-
-
-
-
-
-
-
-
-
-animals = c("dog","cat","fish")
-
-tr = animals[sample(3,100,replace = T)]
-es = animals[sample(3,100,replace = T)]
-metrics(tr,es)
-colnames(d)
+m = metrics(tr,es)
+long_m = gather(m,metrics_name,metric_value,TPR_metric:MKd_metric)
+few_metric = long_m %>% 
+  filter(metrics_name == "TPR_metric" |
+           metrics_name == "TNR_metric" |
+           metrics_name == "ACC_metric" | 
+           metrics_name == "PPV_metric")
+ggplot(few_metric,aes(x=metrics_name,y=metric_value)) + 
+  ggtitle("Recovery of Each Level") +
+  geom_point()+
+  xlab("Metrics Names") +
+  ylab("Measurements") +
+  facet_grid(~Levels_name) + 
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) 
