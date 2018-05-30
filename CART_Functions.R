3fbccf004377d198c5555b7724d55b9edbb2f3e0
Immanuel Williams <jamesijw23@gmail.com>
2018-05-30 08:06:53 -0700

    Create Is number function

--- CART_Functions.R
+++ CART_Functions.R
@@ -2,16 +2,34 @@
 
 ##--------------------------------------
 ## Name: is_number
-## Input: data frame
-## Output: if a the column is numeric
+## Input: df_c --> vector
+## Output: T/F
 ## Purpose: to check if a column is numeric
 ## Improvements: 
+## a) Make sure this function is doing what it is supposed to do
+## compared to python code
+## b) This function subsumes is_number_value function from python
 ##--------------------------------------  
-is_number =function(df){
-  
-  result<- unlist(lapply(df, is.numeric))
+is_number = function(df_c){
+  result<- sum(unlist(lapply(df_c, is.numeric))) == length(df_c)
   return(result)
 }
 
-df1 = c(1,2,4,NA)
-is_number(df1)
+
+
+
+# Function: Performs a Chi_Squared Test or Fisher Exact Test 
+##--------------------------------------
+## Name: chi_squared_test
+## Input: 
+## a) label_df --> Classification df
+## b) feature_df --> features df
+## Output: T/F
+## Purpose: to check if a column is numeric
+## Improvements: 
+##--------------------------------------
+chi_squared_test = function(label_df, feature_df){
+  
+ 
+  return(p_value)
+}
