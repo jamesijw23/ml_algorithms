704e998d109084d1147fcb7ace105934c6793b96
Immanuel Williams <jamesijw23@gmail.com>
2018-05-30 21:27:02 -0700

    Add new dataset to try knn cross validation

--- Testing_Cross_Validation.R
+++ Testing_Cross_Validation.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 df = iris %>% select(Species,Sepal.Length,
                            Sepal.Width,Petal.Length,
                            Petal.Width )
@@ -19,4 +20,19 @@ m = metrics(tr,es)
 long_m = gather(m,metrics_name,metric_value,TPR_metric:MKd_metric)
 
 
-cross_validation_KNN(df,type_cv = 1,p_test=0.2,KN = 3)
+cross_validation_KNN(df,type_cv = 2,p_test=0.2,KN = 3)
+
+
+
+
+
+
+###########
+
+
+beaver_df1 = datasets::beaver1
+beaver_df2 = datasets::beaver2
+beaver_df = rbind(beaver_df1,beaver_df2)
+beaver_df = beaver_df %>% select(activ,time,temp,day)
+
+cross_validation_KNN(beaver_df,type_cv = 1,p_test=0.2,KN = 3)
