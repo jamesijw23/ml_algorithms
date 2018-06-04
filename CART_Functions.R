1bac9f4d5bd9c3254ddf14f7498f19996c483968
Immanuel Williams <jamesijw23@gmail.com>
2018-06-04 11:44:38 -0700

    Create a function that counts the number of each type of label in a dataset.

--- CART_Functions.R
+++ CART_Functions.R
@@ -1,7 +1,11 @@
 
 
 
-
+##--------------------------------------
+## Notes:
+## a) Data Frame needs column name used to depict response varible
+## needs be named Label
+##--------------------------------------
 
 
 
@@ -14,7 +18,7 @@
 ## Output: Unique levels or numbers 
 ## Purpose: Find the unique values for a column in a dataset.
 ## Improvements: 
-  ## a) Less in house Functions
+  ## a) Less in R functions
 ##--------------------------------------
 unique_vals = function(df,col){
   vals = as.character(unique(df[,col]))
@@ -22,3 +26,30 @@ unique_vals = function(df,col){
   return(vals)
 }
 unique_vals(df,4)
+
+
+##--------------------------------------
+## Name: class_counts
+## Input: 
+  ## a) df--> Data Frame
+## Output: A df with column 1 is name column 2 freq.
+## Purpose: Counts the number of each type of label in a dataset.
+## Improvements: 
+  ## a) Less in R functions
+##--------------------------------------
+class_counts = function(df){
+  return(data.frame(table(df$Label)))
+}
+##--------------------------------------
+
+##--------------------------------------
+## Name: is_numeric
+## Input: 
+  ## a) val --> a string or number
+## Output: bool
+## Purpose: Test if a value is numeric.
+## Improvements: 
+  ## a) Less in R functions
+##--------------------------------------
+is_numeric = function()
+is.numeric(3)
