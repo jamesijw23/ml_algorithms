181d8b07a7e2f0437f71dc079afc6bfe9771da45
Immanuel Williams <jamesijw23@gmail.com>
2018-06-04 12:01:30 -0700

    Create an Rcpp file for CART

---
+++ CART_rcpp.cpp
@@ -0,0 +1,32 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+class Location{
+public:
+  Location();
+  Location(int, double);
+  int x;
+  double y;
+  void print();
+};
+
+
+//constructors
+Location::Location() :x(0), y(0) { }
+Location::Location(int xi, double yi) :x(xi), y(yi) { }
+
+//print function
+void Location::print(){
+  Rcpp::Rcout << "x = " << x << std::endl;
+  Rcpp::Rcout << "y = " << y << std::endl;
+}
+
+RCPP_MODULE(locationmodule){
+  Rcpp::class_<Location>( "Location" )
+  .constructor("documentation for default constructor")
+  .constructor<int,double>("documentation for constructor")
+  .field( "x", &Location::x, "documentation for x")
+  .field( "y", &Location::y, "documentation for y")
+  .method( "print", &Location::print, "documentation for print")
+  ;
+}
