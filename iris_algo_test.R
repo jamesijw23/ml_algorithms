803c64187fad94734511d87f470b52705e3f556f
Immanuel Williams <jamesijw23@gmail.com>
2018-06-07 20:36:56 -0700

    SVM added and it works

--- iris_algo_test.R
+++ iris_algo_test.R
@@ -4,6 +4,7 @@ library(class)   ## KNN
 library(rpart)   ## CART
 library(randomForest) ## Random Forest
 library(xgboost)
+library(e1071)
 
 ## testing R's: Logistic Regression, KNN, CART, Random Forest, XGBoost
 
@@ -36,13 +37,15 @@ binary_ml = function(x,y,p){
   ##--------
   # b) Re-format Data for different Shallow Algos
   ##--------
-  x_train = as.matrix(train_df[, 1:4])
-  y_train = as.numeric(factor(train_df[, 5]))-1
+  x_train = as.matrix(train_df %>% select(-y))
+  y_train = train_df %>% select(y)
+  y_train = as.numeric(factor(y_train$y))-1
   
   
   
-  x_test = as.matrix(test_df[, 1:4])
-  y_test = as.numeric(factor(test_df[, 5]))-1
+  x_test = as.matrix(test_df %>% select(-y))
+  y_test = test_df %>% select(y)
+  y_test = as.numeric(factor(y_test$y))-1
   
   
  
@@ -50,11 +53,12 @@ binary_ml = function(x,y,p){
   #################################
   ## ML1: Logistic Regression
   #################################
+  ## Run Model
   log_model = glm(y ~.,
                    data = train_df,
                    family="binomial")
   ## Predict for logistic regression
-  est_log = ifelse(predict(log_model, test_df[,-5], type = "response")>0.5,1,0)
+  est_log = ifelse(predict(log_model, data.frame(x_test), type = "response")>0.5,1,0)
   ## Confusion matrix for logistic regression
   table(y_test,est_log)
   
@@ -63,17 +67,19 @@ binary_ml = function(x,y,p){
   #################################
   ## ML2: KNN
   #################################
-  est_knn = knn(train_df[,-5],test_df[,-5],cl = train_df[,5])
+  ## Run Model
+  est_knn = knn(x_train,x_test,cl = y_train)
   ## Confusion matrix for KNN
-  table(test_df[,5],est_knn)
+  table(y_test,est_knn)
   
   
   #################################
   ## ML3: CART
   #################################
+  ## Run Model
   cart_model = rpart(y ~. , method="class", data = train_df)
   ## Predict for KNN
-  result_cart_test = predict(cart_model,test_df[,-5])
+  result_cart_test = predict(cart_model,data.frame(x_test))
   est_cart = apply(result_cart_test,1,which.max) - 1
   
   ## Confusion matrix for CART
@@ -82,26 +88,41 @@ binary_ml = function(x,y,p){
   
   
   #################################
-  ## Random Forest
+  ## ML4: Random Forest
   #################################
-  rf_model = randomForest(as.factor(Species) ~.,
+  ## Run Model
+  rf_model = randomForest(as.factor(y) ~.,
                           data=train_df, 
                           importance=TRUE, 
                           ntree=2000)
   ## Predict for logistic regression
-  est_rf = predict(rf_model,test_df[-5])
+  est_rf = predict(rf_model,data.frame(x_test))
   ## Confusion matrix for Random Forest
-  table(test_df[,5],est_rf)
+  table(y_test,est_rf)
   
   
   #################################
-  ## XGBoost
+  ## ML5: SVM
   #################################
-
+  ## Run Model
+  svm_model = svm(y ~. , data = train_df)
+  ## Predict for KNN
+  result_svm_test = predict(svm_model,data.frame(x_test))
+  est_svm = apply(result_cart_test,1,which.max) - 1
+  ## Confusion matrix for CART
+  table(y_test,est_svm)
+  
+  
+  
+  #################################
+  ## ML6: XGBoost
+  #################################
   ## Run Model
   xgb_model <- xgboost(data = x_train, label = y_train, nrounds = 10)
   ## Predict for logistic regression
   est_xgb = ifelse(predict(xgb_model,x_test)>0.5,1,0)
   ## Confusion matrix for logistic regression
-  table(test_true,est_xgb)
+  table(y_test,est_xgb)
 }
+
+binary_ml(x,y,0.2)
